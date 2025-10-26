746d9bff8eef16533d89cc0c9f593883b097517c
Alvin Roberts <alvindroberts7@gmail.com>
2025-10-25 20:49:00 -0500

    update Order URL 1

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-const USER_SERVICE_URL = "https://user-service.onrender.com";
+const USER_SERVICE_URL = "https://order-service-w452.onrender.com/";
 const PRODUCT_SERVICE_URL = "https://product-service.onrender.com";
 
 app.get("/api/orders/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const user = await axios.get(`${USER_SERVICE_URL}/api/users/1`);
-    const product = await axios.get(`${PRODUCT_SERVICE_URL}/api/products/1`);
     res.json({
       id,
       user: user.data,
